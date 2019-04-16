489ec33e3e3e17511481613488495779230d38f9
Aceex <Aceex@192.168.1.140>
2019-04-16 09:10:53 -0300

    Primeiro programa do curso.

---
+++ src/br/edu/caelum/fj11/capitulo02/MeuPrograma.java
@@ -0,0 +1,10 @@
+package br.edu.caelum.fj11.capitulo02;
+
+public class MeuPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Voltamos a programa em Java!\n"
+				+ "Então: Hello World Java!");
+	}
+
+}
